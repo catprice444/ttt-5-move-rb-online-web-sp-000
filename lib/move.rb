da8fe310fdd253d4ec8fba8a92a49e1cc9ee7015
Catherine Price <catprice444@gmail.com>
2020-04-06 03:50:32 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -14,6 +14,6 @@ def input_to_index(input)
    input.to_i - 1
 end
 
-def move(board,input,value = "X")
-   board[input.to_1 - 1] = value
+def move(board,index,value = "X")
+   board[index] = value
 end
