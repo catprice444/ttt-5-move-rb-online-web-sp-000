ec84aacc35b81a7bce3b257520e3a0b5d0268902
Catherine Price <catprice444@gmail.com>
2020-04-06 02:05:03 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,11 +6,19 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input)
-   input.to_i - 1
+# code your input_to_index and move method here!
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(user_input)
+   user_input.to_i - 1
+end
 
-def move(board, input, value ="X")
-   board[input] = value
+def move(board, user_input, value="X")
+   board[user_input] = value
 end
